b3a279308af3e950abdd7583389dc21e5acc4f1c
James Nyamu <nyamuj59@gmail.com>
2022-04-16 14:38:07 +0300

    Completed writing the source code for multiprocessing example.

--- src/main/kotlin/Multithreading.kt
+++ src/main/kotlin/Multithreading.kt
@@ -1,14 +1,32 @@
 fun main(args: Array<String>) {
-    val thread1 = Mythread()
+    val thread1 = Mythreads("Thread 1")
     thread1.start()
+
+    val thread2 = Mythreads("Thread 2")
+    thread2.start()
+
+    val thread3 = Mythreads("Thread 3")
+    thread3.start()
+
+    thread3.join()
+
+    println("Threading is done!")
 }
 
-class Mythread():Thread(){
-    var counter = 0
+class Mythreads():Thread(){
+
+    var threadName: String = ""
+
+    constructor(threadName: String): this(){
+        this.threadName = threadName
+        println(this.threadName + " has started")
+    }
+
 
     override fun run(){
+        var counter = 0
         while(counter < 15){
-            println("Count is: $counter")
+            println(this.threadName + " Count is: $counter")
             counter++
 
             try{
